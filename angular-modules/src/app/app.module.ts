6f6704116976b4df792d08a019c804ef5d275c5f
Sergey Vorobyev <s.vorobev@artsofte.ru>
2023-01-19 13:40:14 +0500

    routing-base

--- angular-modules/src/app/app.module.ts
+++ angular-modules/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './components/app/app.component';
 import { MainModule } from './children/main/main.module';
 import { LoggerService } from './services/logger.service';
+import { AppRoutingModule } from "./app-routing.module";
+import { AboutComponent } from './children/about/about.component';
+import { NotFoundComponent } from './children/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    AboutComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
-    MainModule
+    AppRoutingModule,
+    MainModule,
   ],
   providers: [LoggerService],
   bootstrap: [AppComponent]
--- angular-modules/src/app/modules/header/header.module.ts
+++ angular-modules/src/app/modules/header/header.module.ts
@@ -1,6 +1,7 @@
-import { NgModule } from '@angular/core';
+import { InjectionToken, ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from "./header.component";
+export const HEADER_TITLE: InjectionToken<string> = new InjectionToken<string>('Тайтл хедера на страницах выбраного модуля')
 
 @NgModule({
   declarations: [
@@ -12,7 +13,15 @@ import { HeaderComponent } from "./header.component";
   imports: [
     CommonModule
   ],
+  providers: [ {provide: HEADER_TITLE, useValue: 'Главная' } ]
 })
 export class HeaderModule {
-
+  // static forRoot(headerTitle: string): ModuleWithProviders<HeaderModule> {
+  //   return {
+  //     ngModule: HeaderModule,
+  //     providers: [
+  //       {provide: HEADER_TITLE, useValue: headerTitle }
+  //     ]
+  //   }
+  // }
 }
