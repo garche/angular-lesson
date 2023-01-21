5f77d1ecaef428013fb9d3feac0adced46748d1f
Sergey Vorobyev <s.vorobev@artsofte.ru>
2023-01-21 16:44:59 +0500

    lazy-loading

--- angular-modules/src/app/modules/header/header.module.ts
+++ angular-modules/src/app/modules/header/header.module.ts
@@ -1,7 +1,6 @@
 import { InjectionToken, ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from "./header.component";
-export const HEADER_TITLE: InjectionToken<string> = new InjectionToken<string>('Тайтл хедера на страницах выбраного модуля')
 
 @NgModule({
   declarations: [
