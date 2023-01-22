bf9136994cbb234707cad670819d7396df964104
Sergey Vorobyev <s.vorobev@artsofte.ru>
2023-01-22 13:19:46 +0500

    lazy-loading

--- angular-modules/src/app/children/main/components/main/main.component.ts
+++ angular-modules/src/app/children/main/components/main/main.component.ts
@@ -8,25 +8,6 @@ import { LoggerService } from "../../../../services/logger.service";
 })
 export class MainComponent implements OnInit{
 
-  public personList: any[] = [
-    {
-      name: 'Вася',
-      age:  10,
-    },
-    {
-      name: 'Василий',
-      age:  24,
-    },
-    {
-      name: 'Василяндрий',
-      age:  7,
-    },
-    {
-      name: 'Васякинатор',
-      age:  11,
-    },
-  ]
-
   constructor(
     private _loggerService: LoggerService,
   ) {
